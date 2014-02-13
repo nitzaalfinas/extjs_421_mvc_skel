a0affa62222a763193aadb65e162309355ecafa2
nitzaalfinas <nitzaalfinas@gmail.com>
2014-02-13 22:25:21 +0700

    no message

--- app/controller/mainController.js
+++ app/controller/mainController.js
@@ -3,8 +3,27 @@ Ext.define('MvcSkel.controller.mainController', {
     
     
     addDashboard: function() {
-        //
+        // define/create the dashboard panel
+        var dashboardPanel = Ext.create('Ext.panel.Panel',{
+            title: 'Dashboard',
+            itemId: 'dashboardPanel'
+        });
         
+        // define main tab
+        var mainTab = Ext.getCmp('myTabPanel');
+        
+        // define the child tab
+        var dashboardChildTab = mainTab.child('#dashboardPanel');
+        
+        // if dashboard already in mainTab, just set it active
+        if(dashboardChildTab) {
+            mainTab.setActiveTab(dashboardPanel);
+        }
+        // else... means if the dashboardChildTab is not there, add it to mainTab and set it to active
+        else {
+            mainTab.add(dashboardPanel);
+            mainTab.setActiveTab(dashboardPanel);
+        }
     },
     
     
